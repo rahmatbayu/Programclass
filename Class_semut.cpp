549f969f8f1231ba32f71ae45c7d0175da22442c
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-01-17 22:05:22 -0800

    membuat pembentuk output

--- Class_semut.cpp
+++ Class_semut.cpp
@@ -16,3 +16,10 @@ public:
 
     Jenissemut (string namasemut, string habitat, string makanan, string karakter )
     
+      {
+    	Jenissemut::namasemut = namasemut;
+        Jenissemut::habitat = habitat;
+        Jenissemut::makanan = makanan;
+        Jenissemut::karakter = karakter;
+        
+    }
