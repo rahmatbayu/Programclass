c9a8a7cff080ca8125e765769614cda8d60ae6c8
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-01-17 22:50:38 -0800

    output hasil

--- Class_semut.cpp
+++ Class_semut.cpp
@@ -42,10 +42,13 @@ public:
 int main()
 
   { 
-	Jenissemut semutA("semut hitam", "tumpukan batu, daun daun kering yang sudah rusak, memakan serangga pengganggu(hama)", 
-	"lebih aktif di pagi hari dan sore hari");
+	Jenissemut semutA("semut hitam", "tumpukan batu, daun daun kering yang sudah rusak", "memakan serangga pengganggu(hama)", "lebih aktif di pagi hari dan sore hari");
 	
-	Jenissemut semutB("semut merah", "didaunan/tumbuhan, memakan serangga pengganggu(hama)", 
-	"membuat sarang dengan cara mengumpulkan daun daun menjadi sebuah rumah/tempat tinggal");
-	
+	Jenissemut semutB("semut merah", "didaunan/tumbuhan", "memakan serangga pengganggu(hama)", "membuat sarang dengan cara mengumpulkan daun daun menjadi sebuah rumah/tempat tinggal");
+
+	semutA.perolehInfo();
+    
+	semutB.perolehInfo();
+    
+    return 0;	
 }
